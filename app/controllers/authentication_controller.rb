6c121035019184ad7e39e78c7884496592c1fe5c
Matthew Ryan <matthewryan111@yahoo.com>
2014-04-09 18:30:31 +0100

    Books and user login created

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,27 @@
+class AuthenticationController < ApplicationController
+  def sign_in
+    @user = User.new
+  end
+
+  def login
+    username_or_email = params[:user][:username]
+    password = params[:user][:password]
+
+    if username_or_email.rindex('@')
+      email=username_or_email
+      user = User.authenticate_by_email(email, password)
+    else
+      username=username_or_email
+      user = User.authenticate_by_username(username, password)
+    end
+
+    if user
+      flash[:notice] = 'Welcome.'
+      redirect_to :root
+    else
+      flash.now[:error] = 'Unknown user. Please check your username and password.'
+      render :action => "sign_in"
+    end
+
+  end
+end
