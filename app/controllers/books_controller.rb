e327e9ff5eb0cb1206c068de4a73db3675a7bb9e
Matthew Ryan <matthewryan11111@gmail.com>
2014-04-11 11:15:41 +0100

    search functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.search(params[:search])
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,13 @@ class Book < ActiveRecord::Base
   has_many :lineitems
   has_many :orders, :through => :lineitems
 
+  def self.search
+    if search
+      find(:all, :conditions => ['title LIKE ? OR author LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
 
 end
